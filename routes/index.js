f0015dd7290ff52f2d3a29dae66fef4cb145df6b
ManuelSince <ManuelSince@users.noreply.github.com>
2023-02-09 14:45:37 +0100

    add pages styles

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Home', stylesheet: '/stylesheets/home.css' });
 });
-router.get('/product', function(req, res, next) {
-  res.render('product', { title: 'Produit' });
+router.get('/product', function(req, res) {
+  res.render('product', { title: 'Produit', stylesheet: '/stylesheets/product.css' });
 });
-router.get('/data', function(req, res, next) {
-  res.render('data', { title: 'Data dashboard' });
+router.get('/data', function(req, res) {
+  const wetSensor = {
+    x: [],
+    y: []
+  }
+  const lightSensor=[]
+  res.render('data', { title: 'Data dashboard', stylesheet: '/stylesheets/data.css' });
 });
 
 module.exports = router;
